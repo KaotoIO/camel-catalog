bbdbe483745eb398eccd247f434f8b7902ab8451
MAHEK SRIVASTAVA <maheksri444488@gmail.com>
2026-08-01 21:36:57 +0530

    fix(test): close resource stream in CamelYamlDslSchemaProcessorTest (#119)

--- src/test/java/io/kaoto/camelcatalog/generator/CamelYamlDslSchemaProcessorTest.java
+++ src/test/java/io/kaoto/camelcatalog/generator/CamelYamlDslSchemaProcessorTest.java
@@ -32,10 +32,11 @@ class CamelYamlDslSchemaProcessorTest {
     @BeforeEach
     void setUp() throws Exception {
         jsonMapper = new ObjectMapper();
-        var is = YamlRoutesBuilderLoader.class.getClassLoader().getResourceAsStream("schema/camelYamlDsl.json");
+       try(var is = YamlRoutesBuilderLoader.class.getClassLoader().getResourceAsStream("schema/camelYamlDsl.json")){
         ObjectNode yamlDslSchema = (ObjectNode) jsonMapper.readTree(is);
 
         processor = new CamelYamlDslSchemaProcessor(jsonMapper, yamlDslSchema);
+      }
     }
 
     @Test
