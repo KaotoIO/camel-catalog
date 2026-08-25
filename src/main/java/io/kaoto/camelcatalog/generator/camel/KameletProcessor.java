d977b4b5d0a595a6fe48ce2a7fa26b6fcea3141e
Manya56 <163504444+Manya56@users.noreply.github.com>
2026-08-25 17:42:26 +0530

    Refactor KameletProcessor to use constants for keys

--- src/main/java/io/kaoto/camelcatalog/generator/camel/KameletProcessor.java
+++ src/main/java/io/kaoto/camelcatalog/generator/camel/KameletProcessor.java
@@ -23,35 +23,46 @@ public class KameletProcessor {
 
     private KameletProcessor() {
     }
-
+    private static final String SPEC = "spec";
+    private static final String DEFINITION = "definition";
+    private static final String PROPERTIES = "properties";
+    private static final String SLASH_PROPERTIES = "/properties";
+    private static final String TITLE = "title";
+    private static final String DESCRIPTION = "description";
+    private static final String TYPE = "type";
+    private static final String REQUIRED = "required";
+    private static final String ENUM = "enum";
+    private static final String DEFAULT = "default";
+    private static final String FORMAT = "format";
+    
     private static final List<String> TO_STRING_TYPES = List.of("binary");
 
     public static void process(ObjectNode kamelet) {
         var schema = kamelet.withObject("/propertiesSchema");
-        var kameletDef = kamelet.withObject("/spec")
-                .withObject("/definition");
+        var kameletDef = kamelet.withObject(SPEC)
+                .withObject(DEFINITION);
         schema.put("$schema", "http://json-schema.org/draft-07/schema#");
-        schema.put("type", "object");
-        if (kameletDef.has("title")) schema.set("title", kameletDef.get("title"));
-        if (kameletDef.has("description")) schema.set("description", kameletDef.get("description"));
-        if (kameletDef.has("required")) schema.set("required", kameletDef.get("required"));
-        if (kameletDef.has("properties") && !kameletDef.withObject("/properties").isEmpty()) {
-            var kameletProperties = kameletDef.withObject("/properties");
-            var schemaProperties = schema.withObject("/properties");
+        schema.put(TYPE, "object");
+        if (kameletDef.has(TITLE)) schema.set(TITLE, kameletDef.get(TITLE));
+        if (kameletDef.has(DESCRIPTION)) schema.set(DESCRIPTION, kameletDef.get(DESCRIPTION));
+        if (kameletDef.has(REQUIRED)) schema.set(REQUIRED, kameletDef.get(REQUIRED));
+        if (kameletDef.has(PROPERTIES) && !kameletDef.withObject(SLASH_PROPERTIES).isEmpty()) {
+            var kameletProperties = kameletDef.withObject(PROPERTIES);
+            var schemaProperties = schema.withObject(PROPERTIES);
             for (var entry : kameletProperties.properties()) {
                 var name = entry.getKey();
                 var property = entry.getValue();
                 var schemaProperty = schemaProperties.withObject("/" + name);
-                if (property.has("type")) schemaProperty.set("type", property.get("type"));
-                if (TO_STRING_TYPES.contains(property.get("type").asText())) {
-                    schemaProperty.put("$comment", "type:" + property.get("type").asText());
-                    schemaProperty.put("type", "string");
+                if (property.has(TYPE)) schemaProperty.set(TYPE, property.get(TYPE));
+                if (TO_STRING_TYPES.contains(property.get(TYPE).asText())) {
+                    schemaProperty.put("$comment", "type:" + property.get(TYPE).asText());
+                    schemaProperty.put(TYPE, "string");
                 }
-                if (property.has("title")) schemaProperty.set("title", property.get("title"));
-                if (property.has("description")) schemaProperty.set("description", property.get("description"));
-                if (property.has("enum")) schemaProperty.set("enum", property.get("enum"));
-                if (property.has("default")) schemaProperty.set("default", property.get("default"));
-                if (property.has("format")) schemaProperty.set("format", property.get("format"));
+                if (property.has(TITLE)) schemaProperty.set(TITLE, property.get(TITLE));
+                if (property.has(DESCRIPTION)) schemaProperty.set(DESCRIPTION, property.get(DESCRIPTION));
+                if (property.has(ENUM)) schemaProperty.set(ENUM, property.get(ENUM));
+                if (property.has(DEFAULT)) schemaProperty.set(DEFAULT, property.get(DEFAULT));
+                if (property.has(FORMAT)) schemaProperty.set(FORMAT, property.get(FORMAT));
             }
         }
     }
